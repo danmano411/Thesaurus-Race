a87f75c3f35c49cd45033ad6e88d7dd7d04ce569
Dan Mano <danmano411@gmail.com>
2024-08-06 15:10:16 -0400

    fixed goalnav error with spacing

--- app/src/components/GoalNav.tsx
+++ app/src/components/GoalNav.tsx
@@ -29,14 +29,14 @@ const GoalNav:React.FC<GoalNavProps> = ({
 
     return (
         <div className={`fixed w-screen h-screen bg-neutral-500/60 top-0 ${isOpen ? 'opacity-100' : 'opacity-0'} transition duration-200 ${disabled ? 'z-10' : '-z-20'} `}>
-            <div className={`h-full w-1/5 bg-white fixed right-0 flex flex-col p-5 py-16 justify-center items-center gap-32
+            <div className={`h-full w-1/5 bg-white fixed right-0 flex flex-col p-5 py-12 justify-center items-center gap-16
                 transition duration-200 ${isOpen ? 'translate-x-0' : 'translate-x-full'}
             `}>
                 <div className='flex-shrink'>
-                    <h1 className='text-2xl'>{startWord} ---------{`>`} {goalWord}</h1>
+                    <h1 className='text-2xl'>{startWord} --{`>`} {goalWord}</h1>
                 </div>
-                <div className='flex-shrink flex flex-col justify-normal items-center'>
-                    <h1 className='text-5xl font-normal -mb-24'>{goalWord}</h1>
+                <div className='flex-shrink flex flex-col justify-normal items-center gap-4'>
+                    <h1 className='text-5xl font-normal'>{goalWord}</h1>
                     <Sidebar clickable={false} wordInfo={goalWordInfo}/>
                 </div>
             </div>
